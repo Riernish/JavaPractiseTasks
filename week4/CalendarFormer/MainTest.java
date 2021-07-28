3b06c71c571ddab4eb4f54d5730cb2db776bfbc1
Riernish <57988144+Riernish@users.noreply.github.com>
2021-07-28 17:26:12 +0300

    Update MainTest.java

--- week4/CalendarFormer/MainTest.java
+++ week4/CalendarFormer/MainTest.java
@@ -15,11 +15,13 @@ public class MainTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String test = "2001 09 04\n15 01";
+        String test = "2001 09 04\n 15 01";
+        String testFormat = "EEEE, d MMMM yyyy";
         
         try {
             CalendarFormer obj = new CalendarFormer(test);
-            System.out.println(obj.getCalendar());
+            System.out.println(obj.getDate());
+            System.out.println(obj.toFormatString(testFormat));
         } catch (ParseException ex) {
             Logger.getLogger(MainTest.class.getName()).log(Level.SEVERE, null, ex);
         }
